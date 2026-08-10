a65cdfaacfa696569b4677a6c5265e9f40861d04
Ryan de Souza Silva <ryan.souzasilvap1@gmail.com>
2026-08-10 15:56:58 -0300

    feat: contratos de repository e service de Customer

---
+++ AgendamentosAPI/Adapters/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+using AgendamentosAPI.Domain.Ports;
+using AgendamentosAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgendamentosAPI.Adapters.Controllers;
+
+[ApiController]
+[Route("/customers")]
+public class CustomerController(ICustomerService customerService) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> AddCustomer(CustomerInputDto input)
+    {
+        var customer = await customerService.AddCustomerAsync(input);
+        
+        return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetCustomerById(Guid id)
+    {
+        return Ok(await customerService.GetCustomerByIdAsync(id)); 
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateCustomer(Guid id, CustomerInputDto input)
+    {
+        return Ok(await customerService.UpdateCustomerAsync(id, input)); 
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteCustomer(Guid id)
+    {
+        await customerService.DeleteCustomerAsync(id);
+        
+        return NoContent(); 
+    }
+}
